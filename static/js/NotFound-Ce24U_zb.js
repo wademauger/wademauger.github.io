dfb60ea8fdd7be6d9ac0e5ebfcedef96945f777f
Wade Ahlstrom <wadeanthony0100@gmail.com>
2025-10-09 15:47:46 -0700

    Updates

---
+++ static/js/NotFound-Ce24U_zb.js
@@ -0,0 +1,2 @@
+import{j as o,L as e}from"./index-B5oyPn8n.js";const n=()=>o.jsxs("div",{className:"not-found-page",children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"Sorry, the page you are looking for does not exist."}),o.jsx(e,{to:"/",className:"home-link",children:"Return to Home"})]});export{n as default};
+//# sourceMappingURL=NotFound-Ce24U_zb.js.map
